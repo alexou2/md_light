4ea544b21d4c5f14edefed21537b7ef5a993d4d8
alexou2 <alexandreValin2@gmail.com>
2023-09-27 13:41:32 -0400

    added error handling for api errors

--- src/api_error.rs
+++ src/api_error.rs
@@ -1,28 +1,31 @@
-use std::num::ParseIntError;
 use std::fmt;
+use std::num::ParseIntError;
 use tokio::task::JoinError;
 
 #[derive(Debug)]
 pub enum ApiError {
     REQWEST(reqwest::Error),
     JSON(serde_json::Error),
+    STR_ERROR(String),
 }
 impl fmt::Display for ApiError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            ApiError::REQWEST(_) => write!(f, "Error Variant 2 occurred"),
-            ApiError::JSON(_) => write!(f, "Error Variant 2 occurred"),
+            ApiError::REQWEST(_) => write!(f, "request error"),
+            ApiError::JSON(_) => write!(f, "json conversion error"),
+            ApiError::STR_ERROR(_) => write!(f, "error while processing strings"),
         }
     }
 }
 impl std::convert::From<&str> for ApiError {
-    fn from(_: &str) -> Self {
-        todo!()
+    fn from(err: &str) -> Self {
+        // todo!()
+        ApiError::STR_ERROR(err.to_string())
     }
 }
 impl std::convert::From<reqwest::Error> for ApiError {
-    fn from(_: reqwest::Error) -> Self {
-        todo!()
+    fn from(err: reqwest::Error) -> Self {
+        ApiError::REQWEST(err)
     }
 }
 impl std::convert::From<ParseIntError> for ApiError {
@@ -30,9 +33,9 @@ impl std::convert::From<ParseIntError> for ApiError {
         todo!()
     }
 }
-impl std::convert::From<serde_json::Error> for ApiError{
-    fn from(_: serde_json::Error) -> Self {
-        todo!()
+impl std::convert::From<serde_json::Error> for ApiError {
+    fn from(err: serde_json::Error) -> Self {
+        ApiError::JSON(err)
     }
 }
 
@@ -41,13 +44,13 @@ impl std::convert::From<Box<dyn std::error::Error>> for ApiError {
         todo!()
     }
 }
-impl std::convert::From<JoinError> for ApiError{
+impl std::convert::From<JoinError> for ApiError {
     fn from(_: JoinError) -> Self {
         todo!()
     }
 }
-impl std::convert::From<std::string::String> for ApiError{
-    fn from(_: String) -> Self {
-        todo!()
+impl std::convert::From<std::string::String> for ApiError {
+    fn from(err: String) -> Self {
+        ApiError::STR_ERROR(err)
     }
 }
