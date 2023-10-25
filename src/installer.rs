55d5ea4db2efd4cde1337b770898df3504ab815a
alexou2 <alexandreValin2@gmail.com>
2023-10-25 14:03:09 -0400

    added new files for installer

--- src/installer.rs
+++ src/installer.rs
@@ -2,43 +2,43 @@ use reqwest;
 use std::fs::{create_dir, read_dir, write};
 
 use crate::api_error::ApiError;
+const BASE_REPO_URL: &'static str = "https://gitlab.com/_alexou_/md_light/-/raw/master/";
 
 pub async fn install_ressources() -> Result<(), ApiError> {
-    let css_code = download_css().await?;
-    let js_code = download_js().await?;
+    // let css_code = download_css().await?;
+    // let js_code = download_js().await?;
 
+    // checks if the ressources directory is present
     let mut ressource_dir_present = false;
     let dir = "./";
     for entry in read_dir(dir)? {
         let entry = entry?;
         let path = entry.path();
-        if path.is_dir() && path.to_string_lossy()== "./ressources" {
+        if path.is_dir() && path.to_string_lossy() == "./ressources" {
             ressource_dir_present = true;
             break;
         };
     }
-
+    // creates the directory if not present
     if !ressource_dir_present {
         create_dir("./ressources")?;
     }
-    write("./ressources/styles.css", css_code)?;
-    write("./ressources/index.js", js_code)?;
-
+    // the list of files to download
+    let ressource_file_list = vec![
+        "ressources/index.js",
+        "ressources/styles.css",
+        "ressources/profile.svg",
+    ];
+    // downloading and saving the files from the repo
+    for file in ressource_file_list {
+        let buffer = download_file(file).await?;
+        write(file, buffer)?;
+    }
     Ok(())
 }
-async fn download_js() -> Result<String, ApiError> {
-    let js_code =
-        reqwest::get("https://gitlab.com/_alexou_/md_light/-/raw/master/ressources/index.js")
-            .await?
-            .text()
-            .await?;
+async fn download_file(file_path: &str) -> Result<String, ApiError> {
+    let url = format!("{BASE_REPO_URL}{file_path}");
+
+    let js_code = reqwest::get(url).await?.text().await?;
     Ok(js_code)
-}
-async fn download_css() -> Result<String, ApiError> {
-    let css_code =
-        reqwest::get("https://gitlab.com/_alexou_/md_light/-/raw/master/ressources/styles.css")
-            .await?
-            .text()
-            .await?;
-    Ok(css_code)
 }
